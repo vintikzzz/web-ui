8963c27320c5b94a318f48c58f1755855b3feacc
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2022-04-01 03:00:07 +0300

    fix subdomain choosing

--- ui/dist/async_main_7a2bfe28.6f0cfd9522794b8d0273.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[]]);
-//# sourceMappingURL=async_main_7a2bfe28.6f0cfd9522794b8d0273.bundle.js.map
---
+++ ui/dist/async_main_7a2bfe28.72164d748d1289785fd6.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[]]);
+//# sourceMappingURL=async_main_7a2bfe28.72164d748d1289785fd6.bundle.js.map
