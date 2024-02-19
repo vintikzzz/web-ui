48c8a958dc9ff752f7692a3e92926476395361d5
Pavel Tatarskiy <pavel.tatarsky@gmail.com>
2024-02-19 22:00:36 +0300

    fix

--- ui/dist/async_main_7a2bfe28.17b1955634de755f72eb.bundle.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],[]]);
-//# sourceMappingURL=async_main_7a2bfe28.17b1955634de755f72eb.bundle.js.map
---
+++ ui/dist/async_main_7a2bfe28.f42eedd15169a7008db8.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],[]]);
+//# sourceMappingURL=async_main_7a2bfe28.f42eedd15169a7008db8.bundle.js.map
